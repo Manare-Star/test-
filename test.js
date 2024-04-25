70a6bae28bf790719e1c017ceda392f07535d201
Manare <manarestarboy@icloud.com>
2024-04-25 11:32:15 +0200

    added new thingd

---
+++ test.js
@@ -0,0 +1,20 @@
+const darkmode = false;
+const devilmode = false;
+const skymode = true;
+
+
+
+
+if(devilmode)
+{
+    document.body.style.background = "red";
+}
+else if(darkmode)
+{
+    document.body.style.background = "black";
+}
+else{
+    document.body.style.background = "blue";
+}
+
+console.log("done");
